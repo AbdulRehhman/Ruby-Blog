d74e4a1941c3bac1b20d70708b69c9ba97d0b1bc
Rana Abdul Rehman <rana.abdulrehman@pakeventures.com>
2022-05-29 16:30:07 +0500

    Scaffolded Element Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :authors
   scope module: "authors" do
-    resources :posts
+    resources :posts do 
+      resources :elements
+    end
   end
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
